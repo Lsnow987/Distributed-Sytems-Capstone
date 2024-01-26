6dbc6273d99840971ce863220691f9c0219062f9
lsnow987 <lsnow@mail.yu.edu>
2024-01-26 14:32:15 -0500

    added comments

--- VP9-Codec/src/main/java/org/example/Main.java
+++ VP9-Codec/src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) {
+//        https://chat.openai.com/share/b8da0309-7b0a-4231-9dbe-415198dc8cb1 - explanation of commands
         String commands = "# 240p\n" +
                 "mkdir -p videos/240p\n" +
                 "ffmpeg -i input.mp4 -c:v libvpx-vp9 -b:v 400k -maxrate 500k -bufsize 1000k -c:a libopus -b:a 64k -threads 8 -tile-columns 2 -frame-parallel 1 -g 128 -keyint_min 60 -s 426x240 -profile:v 0 -f webm -dash 1 -slices 4 -map 0 -f segment -segment_time 10 -segment_format webm -segment_list videos/240p/playlist.m3u8 -segment_list_flags +live -segment_list_type m3u8 -segment_list_size 720 -segment_list_entry_prefix videos/240p/segment_ videos/240p/segment_%05d.webm\n" +
